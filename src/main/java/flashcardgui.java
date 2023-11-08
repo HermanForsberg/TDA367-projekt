8e2fb4b2a292cfc3fa6095d602e1100ea6e065d1
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2023-11-08 14:51:11 +0100

    flashcardgui

---
+++ src/main/java/flashcardgui.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class flashcardgui {
+    private JPanel panel1;
+    private JTextField textField1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
