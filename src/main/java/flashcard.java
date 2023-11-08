698103d0c60d0ff5e5205c84afdda9634717a8d2
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2023-11-08 14:10:10 +0100

    flashcards implementation and git-ignoring workspace

---
+++ src/main/java/flashcard.java
@@ -0,0 +1,27 @@
+public class flashcard {
+    private String solution;
+    private String question;
+    private boolean flipped;
+    private boolean correct;
+
+    public flashcard(String question, String solution){
+        this.question = question;
+        this.solution = solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void flip(){
+        this.flipped = !this.flipped;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+}
---
+++ src/main/java/flashcardDeck.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class flashcardDeck {
+    private Set<flashcard> deck;
+    private String deckName;
+    private int size;
+
+    public flashcardDeck(String deckName) {
+        this.deck = new HashSet<flashcard>();
+        this.deckName = deckName;
+        this.size = 0;
+    }
+
+    public boolean addFlashcard(flashcard flash){
+        boolean added = this.deck.add(flash);
+        if(added){
+            this.size ++;
+        }
+        return added;
+    }
+
+    public boolean delete(flashcard flash){
+        boolean deleted = this.deck.remove(flash);
+        if(deleted){
+            this.size --;
+        }
+        return deleted;
+    }
+
+    public void setDeckName(String deckName) {
+        this.deckName = deckName;
+    }
+
+    public Set<flashcard> getDeck() {
+        return deck;
+    }
+
+    public String getDeckName() {
+        return deckName;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
