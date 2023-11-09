f523e2666e2923750bb6817dc5cc95838f2c8a72
HugoUnosson <hugo.unosson@gmail.com>
2023-11-09 14:16:00 +0100

    Created the backend for Stopwatch

---
+++ src/main/java/Stopwatch.java
@@ -0,0 +1,66 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Stopwatch {
+    //Timer     = Keeps track of the time in a background thread.
+    //TimerTask = Contains an abstract method called run(). When our Timer reaches a certain time
+    //            it will execute a task either once or repeatedly.
+    private int minutes;
+    private int seconds;
+    private Timer timer;
+    public Stopwatch(int minutes) {
+
+        this.minutes = minutes;
+        this.seconds = 0;
+        this.timer = new Timer();
+
+    }
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            if (minutes == 0 && seconds == 0){
+                timer.cancel();
+            }
+            else {
+                if (seconds == 0){
+                    minutes --;
+                    seconds = 59;
+                }
+                else {
+                    seconds --;
+                }
+            }
+            System.out.printf("%02d:%02d%n", minutes, seconds);
+        }
+    };
+
+    public void startClock(){
+        timer.scheduleAtFixedRate(task, 0, 1000); // 1000ms = 1s
+    }
+    public void resetClock(){
+    }
+    public void pauseClock(){
+        timer.cancel();
+    }
+
+    private void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    private void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public static void main(String[] args){
+        Stopwatch stopwatch = new Stopwatch(10);
+        stopwatch.startClock();
+    }
+}
