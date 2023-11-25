831fe0350518bc33c87d73a730e5c2323690a13a
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2023-11-25 10:32:40 +0100

    yessir

--- src/main/java/Controller/MiniFlashcard.java
+++ src/main/java/Controller/MiniFlashcard.java
@@ -8,10 +8,14 @@ import java.awt.*;
 public class MiniFlashcard extends JPanel {
     public MiniFlashcard(Flashcard flashcard) {
         setLayout(new GridBagLayout());
-        JLabel question = new JLabel(flashcard.getQuestion());
-        JLabel solution = new JLabel(flashcard.getSolution());
-        add(question);
-        add(solution);
+        GridBagConstraints c = new GridBagConstraints();
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        c.gridy = 0;
+        JLabel question = new JLabel("Question: " + flashcard.getQuestion());
+        add(question,c);
+        c.gridy = 1;
+        JLabel solution = new JLabel("Solution: " + flashcard.getSolution());
+        add(solution,c);
     }
 
 }
