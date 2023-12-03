248b1a9d5e72e42a4bd14d3e5a267ce0832c5d01
LudwigAl <alexandersson_ludwig@yahoo.se>
2023-12-03 21:21:33 +0100

    Skapat profil.

---
+++ src/main/java/Controller/ProfileController.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import Model.Profile;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ProfileController extends JButton {
+
+    public ProfileController(Profile model){
+        setText(model.getName());
+
+    }
+}
