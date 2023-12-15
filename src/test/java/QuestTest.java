c89d904e801c5e77086a313d742573f73ef02da0
HugoUnosson <hugo.unosson@gmail.com>
2023-12-15 12:10:43 +0100

    QuestTest now works

--- src/test/java/QuestTest.java
+++ src/test/java/QuestTest.java
@@ -1,18 +1,12 @@
 import Model.Quests.Quest;
-import Model.Quests.QuestFactory;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 
 public class QuestTest {
     @Test
     public void update_progress_updates_progress_correctly(){
-        QuestFactory questFactory = new QuestFactory();
-        ArrayList<Quest> quests = questFactory.getQuests();
-        Quest quest = quests.get(0);
+        Quest quest = new Quest("Daily","Clock", "Testing clock", 5, 35, 100);
         quest.updateProgress(20);
         assertTrue(quest.getProgress()>0);
     }
